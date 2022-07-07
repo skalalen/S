da0e8083b7a8ace5cb3954cbc6b3705601c4756f
skalalen <108368630+skalalen@users.noreply.github.com>
2022-07-07 00:39:57 -0500

    S
    SS

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0x618b4144685E8F3a0F5AdF4293DCE1EB958F512F";
+export const STAKINGCONTRACT = "0xbb6f6DA51e9A45DB81D87fa118F8A0154D3d4D3B";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmPtPF6frqAsHPT8mWVVAWkQf8dG5bDHSTD6nhhgTGZgMM/";
+export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/cE3yEXl6jD672JZrTtm_g3JjWjSGX_kH");
